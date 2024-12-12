004f7d28ce33bd8c2ce8745812050555d3ab3619
Aira Estur <airaaltisestur@gmail.com>
2024-12-12 23:27:03 +0800

    added two data sets

--- python_files/main_tool.py
+++ python_files/main_tool.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     model_filepath = "trained_chord_detector.pth"
 
     # Path to the input .wav file
-    input_wav = r"D:\ComputerEngineering\RESEARCH\data_training\data_sets\Extracted_Chords\A_minor\Minor-A_Miguel.wav"
+    input_wav = r"D:\ComputerEngineering\RESEARCH\data_training\data_sets\Extracted_Chords\G_major\Major-G_Marc.wav"
 
     # Load the model and encoder
     model, encoder = load_model(model_filepath)
